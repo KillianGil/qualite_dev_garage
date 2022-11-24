e40f5341752f5d236d80e979b74f1f55afecf789
TheGreatDuke <clement.dugourd@etu.univ-amu.fr>
2022-11-24 16:57:16 +0100

    fiat, ford, nissan, peugeit, toyota, tesla, mercedes, renault

--- Model.java
+++ Model.java
@@ -134,7 +134,230 @@ public enum Model {
         Moteur getMoteur() {
             return new Moteur(16, 265, 9.3, Propultion.ESSENCE);
         }
+    },
+    CINQ_CENT_ABARTH{
+        Marque getMarque() {
+            return Marque.FIAT;
+        }
+
+        Roue getRoue() {
+            return new Roue(Jante.DIX_SEPT_POUCES, MarquePneu.PIRELLI);
+        }
+
+        Type getType(){
+            return Type.SPORT;
+        }
+
+        Moteur getMoteur() {
+            return new Moteur(9, 160, 6.6, Propultion.ESSENCE);
+        }
+    },
+    MULTIPLA{
+        Marque getMarque() {
+            return Marque.FIAT;
+        }
+
+        Roue getRoue() {
+            return new Roue(Jante.QUINZE_POUCES, MarquePneu.PIRELLI);
+        }
+
+        Type getType(){
+            return Type.FOURGONNETTE;
+        }
+
+        Moteur getMoteur() {
+            return new Moteur(7, 120, 1.9, Propultion.DIESEL);
+        }
+    },
+    MUSTANG_SOIXANTE_NEUF{
+        Marque getMarque() {
+            return Marque.FORD;
+        }
+
+        Roue getRoue() {
+            return new Roue(Jante.QUINZE_POUCES, MarquePneu.CONTINANTAL);
+        }
+
+        Type getType(){
+            return Type.SPORT;
+        }
+
+        Moteur getMoteur() {
+            return new Moteur(27, 330, 17.7, Propultion.ESSENCE);
+        }
+    },
+    EXPLORER{
+        Marque getMarque() {
+            return Marque.FORD;
+        }
+
+        Roue getRoue() {
+            return new Roue(Jante.VINGT_POUCES, MarquePneu.CONTINANTAL);
+        }
+
+        Type getType(){
+            return Type.SUV;
+        }
+
+        Moteur getMoteur() {
+            return new Moteur(32, 457, 3.1, Propultion.HYBRIDE);
+        }
+    },
+    PRIMASTAR_COMBI{
+        Marque getMarque() {
+            return Marque.NISSAN;
+        }
+
+        Roue getRoue() {
+            return new Roue(Jante.SEIZE_POUCES, MarquePneu.GOODYEAR);
+        }
+
+        Type getType(){
+            return Type.MINIBUS;
+        }
+
+        Moteur getMoteur() {
+            return new Moteur(8, 150, 7.2, Propultion.DIESEL);
+        }
+    },
+    JUKE{
+        Marque getMarque() {
+            return Marque.NISSAN;
+        }
+
+        Roue getRoue() {
+            return new Roue(Jante.DIX_NEUF_POUCES, MarquePneu.GOODYEAR);
+        }
+
+        Type getType(){
+            return Type.BREAK;
+        }
+
+        Moteur getMoteur() {
+            return new Moteur(6, 114, 5.8, Propultion.SP95);
+        }
+    },
+    YARIS{
+        Marque getMarque() {
+            return Marque.TOYOTA;
+        }
+
+        Roue getRoue() {
+            return new Roue(Jante.SEIZE_POUCES, MarquePneu.GOODYEAR);
+        }
+
+        Type getType(){
+            return Type.BERLINE;
+        }
+
+        Moteur getMoteur() {
+            return new Moteur(4, 92, 5.7, Propultion.SP95);
+        }
+    },
+    MODEL_S{
+        Marque getMarque() {
+            return Marque.TESLA;
+        }
+
+        Roue getRoue() {
+            return new Roue(Jante.DIX_NEUF_POUCES, MarquePneu.CONTINANTAL);
+        }
+
+        Type getType(){
+            return Type.SPORT;
+        }
+
+        Moteur getMoteur() {
+            return new Moteur(4, 1020, 0, Propultion.ELECTRIQUE);
+        }
+    },
+    MODEL_Y{
+        Marque getMarque() {
+            return Marque.TESLA;
+        }
+
+        Roue getRoue() {
+            return new Roue(Jante.DIX_NEUF_POUCES, MarquePneu.CONTINANTAL);
+        }
+
+        Type getType(){
+            return Type.SUV;
+        }
+
+        Moteur getMoteur() {
+            return new Moteur(11, 462, 0, Propultion.ELECTRIQUE);
+        }
+    },
+    CLASSE_C{
+        Marque getMarque() {
+            return Marque.MERCEDES;
+        }
+
+        Roue getRoue() {
+            return new Roue(Jante.DIX_HUIT_POUCES, MarquePneu.BRIDGESTONE);
+        }
+
+        Type getType(){
+            return Type.BERLINE;
+        }
+
+        Moteur getMoteur() {
+            return new Moteur(11, 204, 6.5, Propultion.HYBRIDE);
+        }
+    },
+    CLASSE_G{
+        Marque getMarque() {
+            return Marque.MERCEDES;
+        }
+
+        Roue getRoue() {
+            return new Roue(Jante.DIX_HUIT_POUCES, MarquePneu.BRIDGESTONE);
+        }
+
+        Type getType(){
+            return Type.SUV;
+        }
+
+        Moteur getMoteur() {
+            return new Moteur(17, 245, 9.9, Propultion.DIESEL);
+        }
+    },
+    KANGOO{
+        Marque getMarque() {
+            return Marque.RENAULT;
+        }
+
+        Roue getRoue() {
+            return new Roue(Jante.QUINZE_POUCES, MarquePneu.MICHELIN);
+        }
+
+        Type getType(){
+            return Type.FOURGONNETTE;
+        }
+
+        Moteur getMoteur() {
+            return new Moteur(7, 114, 6.2, Propultion.ESSENCE);
+        }
+    },
+    ARKANA{
+        Marque getMarque() {
+            return Marque.RENAULT;
+        }
+
+        Roue getRoue() {
+            return new Roue(Jante.DIX_HUIT_POUCES, MarquePneu.MICHELIN);
+        }
+
+        Type getType(){
+            return Type.CROSSOVER;
+        }
+
+        Moteur getMoteur() {
+            return new Moteur(7, 140, 4.9, Propultion.ESSENCE);
+        }
     };
+
+
 
     abstract Moteur getMoteur();
 
