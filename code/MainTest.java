d2d38e01c3133e6ead527121eb7c436d0ec87df9
TheGreatDuke <clement.dugourd@etu.univ-amu.fr>
2022-12-05 17:24:15 +0100

    Création class Garage

--- code/MainTest.java
+++ code/MainTest.java
@@ -1,7 +1,10 @@
 package code ;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class MainTest {
-    public static void main(String[] argvs){
+    public static void main(String[] argvs) {
         VoitureFactory voiture = new VoitureFactory();
         Voiture centSix = VoitureFactory.newVoiture(Modele.CENT_SIX);
         System.out.println(centSix);
